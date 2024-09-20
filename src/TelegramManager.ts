6c2c1f3b72004b4bc4546f13ec1959d949c6b48b
SSK <sheetysai>
2024-09-20 12:02:54 +0530

    Chnages

--- src/TelegramManager.ts
+++ src/TelegramManager.ts
@@ -55,7 +55,7 @@ class TelegramManager {
     async createClient(handler = true): Promise<TelegramClient> {
         try {
             //console.log("Creating Client: ", this.clientDetails.clientId)
-            const result2 = <any>await fetchWithTimeout(`https://checker-production-8f93.up.railway.app/forward/archived-clients/fetchOne/${this.clientDetails.mobile}`);
+            const result2 = <any>await fetchWithTimeout(`https://checker-production-c3c0.up.railway.app/forward/archived-clients/fetchOne/${this.clientDetails.mobile}`);
             // //console.log("ArchivedClient : ", result2.data)
             this.client = new TelegramClient(new StringSession(result2.data.session), parseInt(process.env.API_ID), process.env.API_HASH, {
                 connectionRetries: 5,
