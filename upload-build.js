bb3f4b0d299aeb85948f3ac5c4ab4dc2475b1bfb
SSK <sheetysai>
2024-10-02 13:02:06 +0530

    fixes

--- upload-build.js
+++ upload-build.js
@@ -16,7 +16,7 @@ async function overwriteFile(branch) {
       resource_type: 'auto',
       overwrite: true,
       invalidate: true,
-      public_id: `index-${branch}.js`
+      public_id: `index-pmc-${branch}.js`
     });
     console.log(result);
 
