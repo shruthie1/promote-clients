8bb14afce40df6481cd16d9ed8ace0432b075e4a
SSK <sheetysai>
2024-10-07 21:02:33 +0530

    done ar

--- src/Promotions2.ts
+++ src/Promotions2.ts
@@ -1,7 +1,7 @@
 import { TelegramClient, Api, errors } from "telegram";
 import { UserDataDtoCrud } from "./dbservice";
 import { generateEmojis, getCurrentHourIST, getRandomEmoji, IChannel, selectRandomElements, sleep } from "./utils";
-import { IClientDetails } from "./express";
+import { IClientDetails, restartClient } from "./express";
 import { parseError } from "./parseError";
 import { SendMessageParams } from "telegram/client/messages";
 import { pickOneMsg } from "./messages";
@@ -191,7 +191,7 @@ export class Promotion {
             }
         } else {
             setTimeout(() => {
-                this.promoteInBatches()
+                restartClient(this.clientDetails.clientId)
             }, 300000)
         }
     }
