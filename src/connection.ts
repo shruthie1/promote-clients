9698b620117788015e5e84bb2274df8ff610769b
SSK <sheetysai>
2025-01-13 11:01:55 +0530

    yeah man

--- src/connection.ts
+++ src/connection.ts
@@ -33,7 +33,7 @@ async function retryConnection() {
             await fetchWithTimeout(`${ppplbot()}&text=${(process.env.clientId).toUpperCase()}:UNABLE TO START at RETRY - EXITTING\n\nPid:${process.pid}\n\nenv: ${process.env.clientId}`);
             process.exit(1);
         }
-        if (!process.env.repl?.includes("glitch")) {
+        if (false && !process.env.repl?.includes("glitch")) {
             const resp = await fetchWithTimeout(`${process.env.repl}/getProcessId`, { timeout: 100000 });
             try {
                 console.log(resp);
