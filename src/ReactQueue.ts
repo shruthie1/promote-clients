e774d9a5724383416f8ef4245aa999beb026015d
SSK <sheetysai>
2025-01-21 20:05:14 +0530

    iji

--- src/ReactQueue.ts
+++ src/ReactQueue.ts
@@ -1,7 +1,7 @@
 export class ReactQueue {
     private static instance: ReactQueue;
     public items: string[] = [];
-    private maxSize = 7;
+    private maxSize = 9;
     private timer: NodeJS.Timeout;
 
     private constructor() { }
