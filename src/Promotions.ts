4c8a4c86a1ffb3900b6a4a6ff384577df503735e
SSK <sheetysai>
2025-01-22 21:17:27 +0530

    wdjh

--- src/Promotions.ts
+++ src/Promotions.ts
@@ -267,8 +267,8 @@ export class Promotion {
             const isMorning = (hour > 9 && hour < 22);
             const offset = Math.floor(Math.random() * 3);
             const randomIndex = selectRandomElements(channelInfo.availableMsgs, 1)[0] || '0';
-            const endMsg = pickOneMsg([this.promoteMsgs[randomIndex], '      U bussy👀?', '      Trry Once!!😊💦', '      Waiiting fr ur mssg.....Dr!!💦', '      U Onliine?👀', "      I'm Avilble!!😊", '      U Bussy??👀💦', '      U Intrstd??👀💦',       'U Awakke?👀💦', '      U therre???💦💦']);
-            const msg = `     **${pickOneMsg(greetings)}_._._._._._._!!**${emojis}\n\n\n\n**${endMsg}**`;
+            const endMsg = pickOneMsg([this.promoteMsgs[randomIndex], '      U bussy👀?', 'Trry Once!!😊💦', 'Waiiting fr ur mssg.....Dr!!💦', 'U Onliine?👀', "I'm Avilble!!😊", 'U Bussy??👀💦', 'U Intrstd??👀💦', 'U Awakke?👀💦', 'U therre???💦💦']);
+            const msg = `**${pickOneMsg(greetings)}_._._._._._._!!**${emojis}\n\n\n\n**${endMsg}**`;
             // const addon = (offset !== 1) ? `${(offset === 2) ? `**\n\n\n             TODAAY's OFFFER:\n-------------------------------------------\n𝗩𝗲𝗱𝗶𝗼 𝗖𝗮𝗹𝗹 𝗗𝗲𝗺𝗼 𝗔𝘃𝗶𝗹𝗯𝗹𝗲${randomEmoji}${randomEmoji}\n𝗩𝗲𝗱𝗶𝗼 𝗖𝗮𝗹𝗹 𝗗𝗲𝗺𝗼 𝗔𝘃𝗶𝗹𝗯𝗹𝗲${randomEmoji}${randomEmoji}\n-------------------------------------------**` : `**\n\nJUST Trry Once!!😚😚\nI'm Freee Now!!${generateEmojis()}`}**` : `${generateEmojis()}`;
             // console.log(`Selected Msg for ${channelInfo.channelId}, ${channelInfo.title} | ChannelIdex:${this.channelIndex} | MsgIndex: ${randomIndex}`);
             sentMessage = await this.sendMessageToChannel(mobile, channelInfo, { message: `${msg}` });
