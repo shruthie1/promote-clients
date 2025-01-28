7388c26b02fa4627c99b989391aa2e84d4757597
SSK <sheetysai>
2025-01-28 17:42:39 +0530

    widjo

--- src/Promotions.ts
+++ src/Promotions.ts
@@ -273,7 +273,7 @@ export class Promotion {
             if (msgFlag) {
                 endMsg = pickOneMsg(['               **U bussy👀?**', '               **Trry Once!!😊💦**', '**Waiiting fr ur mssg.....Dr!!💦**', '             **U Onliine?👀**', "         **I'm Avilble!!😊**", '               **U Intrstd??👀💦**', '             **U Awakke?👀💦**', '          **U therre???💦💦**']);
             }
-            const addon = (offset !== 1) ? `${(offset === 2) ? `**\n\n\n             TODAAY's OFFFER:\n-------------------------------------------\n𝗩𝗲𝗱𝗶𝗼 𝗖𝗮𝗹𝗹 𝗗𝗲𝗺𝗼 𝗔𝘃𝗶𝗹𝗯𝗹𝗲${randomEmoji}${randomEmoji}\n𝗩𝗲𝗱𝗶𝗼 𝗖𝗮𝗹𝗹 𝗗𝗲𝗺𝗼 𝗔𝘃𝗶𝗹𝗯𝗹𝗲${randomEmoji}${randomEmoji}\n-------------------------------------------**` : `**\n\nJUST Trry Once!!😚😚\nI'm Freee Now!!${generateEmojis()}`}**` : `${generateEmojis()}`;
+            const addon = (offset !== 1) ? `${(offset === 2) ? `**\n\n\n             TODAAY's OFFFER:\n-------------------------------------------\n𝗩𝗲𝗱𝗶𝗼 𝗖𝗮𝗹𝗹 𝗗𝗲𝗺𝗼 𝗔𝘃𝗶𝗹𝗯𝗹𝗲${randomEmoji}${randomEmoji}\n𝗩𝗲𝗱𝗶𝗼 𝗖𝗮𝗹𝗹 𝗗𝗲𝗺𝗼 𝗔𝘃𝗶𝗹𝗯𝗹𝗲${randomEmoji}${randomEmoji}\n-------------------------------------------**` : `**\n\nJUST Trry Once!!😚😚\nI'm Freee Now!!${generateEmojis()}`}**` : endMsg;
             const msg = `**${pickOneMsg(greetings)}_._._._._._._!!**${emojis}\n\n\n\n${addon}`;
             // console.log(`Selected Msg for ${channelInfo.channelId}, ${channelInfo.title} | ChannelIdex:${this.channelIndex} | MsgIndex: ${randomIndex}`);
             sentMessage = await this.sendMessageToChannel(mobile, channelInfo, { message: `${msg}` });
